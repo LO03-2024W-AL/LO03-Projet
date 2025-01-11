30c40d22e51fb69742bb688190ee76cb3beeca82
YYuX-1145 <138500330+YYuX-1145@users.noreply.github.com>
2025-01-11 11:09:02 +0800

    Add files via upload

--- pcrontab.sh
+++ pcrontab.sh
@@ -1,33 +1,58 @@
 #!/bin/bash
+
+# match($1=proposed_time_value,$2=current_time_value)->state:int -1=err,0=false,1=true
 function match {
     case $1 in         
         *-*)
             start=$(cut -d '-' -f 1 <<<$1)
             end=$(cut -d '-' -f 2 <<<$1)
-            echo "$start to $end"
+            if [ $start -gt $end ];then
+                echo "start $start greater than end $end !"
+                return -1
+            fi
+            if [ $2 -ge $start && $2 -le $end ];then
+                return 1
+            else
+                return 0
+            fi
             ;;
         [0-9]|[0-9][0-9])
-            echo "single number $1"
+            if [ $2 -eq $1 ];then
+                return 1
+            else
+                return 0
+            fi
             ;;
         '*')
-            echo "any"
+            return 1
             ;;
 
         *:*)
             IFS=':' read -r -a values <<< "$1"
-            echo "${values[*]}"
+            #echo "${values[*]}"
+            for val in $values ; do
+                if [ $val -eq $2 ];then
+                    return 1
+                fi
+            done
+            return 0
             ;;
         
         '*/'*)
-            echo "step $1"
-            x=$1
-            step=${x:2}
+            #echo "step $1"
+            step=${arg#*/}
+            if [ $(( $2 % step )) -eq 0 ];then
+                    return 1
+            fi
+            return 0
             ;;
         *)
             echo "unknown: $1"
+            return -1
             ;;
     esac
 }
+
 #resolve arguments
 #pcrontab [-u user] {-l | -r | -e} manage the tasks
 #0 30 7 * * 1-5 commande
@@ -50,11 +75,16 @@ fi
 case $1 in
     -e)
             shift
-            echo "$@"
+            cmd="$*"
             for(( i=1;i<=6;i++ ));do
-                match "$1"
+            match "$1" "0"
+                if [ $? -eq -1 ];then
+                    echo "error"
+                    exit 1
+                fi
                 shift
             done
+            echo "$cmd" >> "tmp/${user_name}Pcrontab" #
             ;;
     -*|--*)  # get unknown args
             echo "unknown args: $1"
@@ -62,3 +92,33 @@ case $1 in
             ;;
 esac
 
+function main {
+    while true;do    
+        for usr in $(ls "tmp");do
+            current_time=$(date "+%S %M %H %d %m %u")
+            while read -r line;do
+                IFS=' ' read -r -a ligne <<< "$line"
+                #echo "$*"
+                m=0
+                for val in $current_time;do
+                    match "${ligne[$m]}" "$val"
+                    if [ $? -eq 1 ];then
+                        ((m++))
+                    else
+                        break
+                    fi
+                done
+                if [ $m -eq 6 ];then
+                    cmd=""
+                    for (( i=6; i<${#ligne[@]}; i++ )); do
+                        cmd+="${ligne[$i]} "
+                    done
+                    echo "execute: $cmd"
+                    eval "$cmd" &
+                fi
+            done < "tmp/${usr}"
+            sleep 1
+        done
+    done
+}
+main
